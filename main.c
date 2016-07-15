69587ab240d11b1c09d2c612dcc7464d827bf823
Voldie <voldemor2009@hotmail.com>
2016-07-15 13:36:59 +0200

    Added additional optional arguments.

--- main.c
+++ main.c
@@ -30,35 +30,67 @@ unsigned long long int compute_wilson_decimal_prime(unsigned long long int p){
 		/*	(tmp * i) mod p === r*/
 		tmp = (tmp * i) % p;
 	}
+
 	return (tmp + 1) % p;
 }
 
 int main(int argc, char** argv){
 	unsigned long long int isPrime = 1;
+	unsigned long long int tmp;
 	unsigned int c;
+	unsigned int base = 10;
+	unsigned int human = 0;
 
 	if(argc <= 1){
 		return EXIT_FAILURE;
 	}
 
-
-	while(( c = getopt(argc, argv, ":d:v")) != EOF){
+	while(( c = getopt(argc, argv, ":d:vH:o:b:h")) != EOF){
 		switch(c){
 		case 'v':
 			printf("version 1.0.0. \n");
 			return EXIT_SUCCESS;
 		break;
-		case 'd':
-		default:{
+		case 'h':
+			human = 1;
+			break;
+		case 'b':
 			if(optarg){
-				unsigned long long int tmp = strtoll(optarg, NULL, 10);
-				isPrime = compute_wilson_decimal_prime(tmp);
-				printf("%lld is prime : %s\n", tmp, isPrime == 0 ? "true" : "false");
+				base = atoi(optarg);
 			}
-		}break;
+			break;
+		case 'o':
+			if(optarg){
+				tmp = strtoll(optarg, NULL, 8);
+
+			}
+			break;
+
+		case 'H':
+			if(optarg){
+				tmp = strtoll(optarg, NULL, 16);
+
+			}
+			break;
+		case 'd':
+		default:
+			if(optarg){
+				tmp = strtoll(optarg, NULL, 10);
+			}
+			break;
 		}
+
+	}
+	if(optind < argc){
+		tmp = strtoll(argv[optind], NULL, base);
 	}
 
+	/*	compute prime.*/
+	isPrime = compute_wilson_decimal_prime(tmp);
+	if(!human)
+		printf("%d\n", isPrime == 0 ? 0 : 1);
+	else
+		printf("%lld is prime : %s\n", tmp,  isPrime == 0 ? "true" : "false");
 
 	return EXIT_SUCCESS;
 }
